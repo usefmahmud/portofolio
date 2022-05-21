b36cac6da35a9b08608f074dcfd9e2ceb489189f
Yousef Mahmoud <usef.mahmoud@outlook.com>
2022-05-21 10:44:55 +0200

    Add projects in DB

--- src/DB/projects.js
+++ src/DB/projects.js
@@ -2,49 +2,118 @@ export const projects = [
     {
         name: 'RWTON',
         type: 'branding',
-        breif: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f2ba8b142734611.626c1be9bff20.png',
+        breif: "",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/63b3e5142734611.626c1be9bb520.png',
         images: [
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f2ba8b142734611.626c1be9bff20.png',
-        ],
-        slug: 'yousef mahmoud branding'
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/66cd4f142734611.626c1be9baa2c.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/399c63142734611.626c1d8914f02.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a82b71142734611.626c1be9bf0c2.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e9afa7142734611.626c1be9bf69d.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/63b3e5142734611.626c1be9bb520.png'
+        ]
+    },
+    {
+        name: 'LogoFolio VOl.1',
+        type: 'logofolio',
+        breif: "Collection of logos i made in 2022",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e7ed19142445145.62672c99894b0.png',
+        images: [
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f651c9142445145.62672c998a5da.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0ca72b142445145.62672c998b90d.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f2d080142445145.62672c9986ac3.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d7e0fe142445145.62672c99874d6.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/1ce2c7142445145.62672c9987cd3.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/628272142445145.62672d30ad30d.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e2b737142445145.62672d30abfbd.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/2ba319142445145.62672d30acb48.png'
+        ]
     },
     {
         name: 'KAJARIA',
         type: 'branding',
-        breif: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e55683142342275.6265680639898.png',
+        breif: "KAJARIA is a modern furniture store.It is interested in attracting a group of young people who love simplicity and modernity.We tried to design the logo to be simple and easy to understand to be more clear",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/4d6b1a142342275.626568063923c.png',
         images: [
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-        ],
-        slug: 'yousef mahmoud branding'
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/87273d142342275.626568063749e.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3472e4142342275.6265680635ea8.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/8223af142342275.6265680637987.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/186c9b142342275.6265680636d62.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/4d6b1a142342275.626568063923c.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/eb07ec142342275.6265680639ff8.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/387b2c142342275.626568063aa34.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/19d4a5142342275.62656806383ba.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/69ec75142342275.6265680638b7a.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/cecacf142342275.626568063a487.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/09590e142342275.6265680637f1f.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a57753142342275.62656806365f7.png'
+        ]
+    },
+    {
+        name: 'Gandesokinium Club',
+        type: 'branding',
+        breif: "The Gandesokinium Club is a student science activity for high school and college students that shows them what it's like to be a real leader by creating interviews with many experts and scientists around the world.",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/7e1d52139832441.6236ffb0a0b0f.png',
+        images: [
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/5c87a7139832441.6236ffb09e820.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b302a8139832441.6236ffb09e271.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0b8a9c139832441.6236ffb09efb6.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/abd10f139832441.6236ffb09d807.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/7e1d52139832441.6236ffb0a0b0f.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f3d261139832441.6236ffb0a17ad.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e28a74139832441.6236ffb09fd5f.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/c013e1139832441.6236ffb09f6a5.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/2dddf5139832441.623b82a65a1e0.png'
+        ]
     },
     {
         name: 'Yousef Mahmoud',
         type: 'branding',
-        breif: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
+        breif: "The Idea: I tried to make the logo as simple and modern as possible. Therefore, I relied on its design on one of the basic shapes in graphic design, which is the square. The design is inspired by the layout icon and expresses the letter Y, which is the first letter of my name",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/37cd5b139345117.622e44eb51888.png',
         images: [
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-        ],
-        slug: 'yousef mahmoud branding'
-    },{
-        name: 'Gandesokinium Club',
-        type: 'branding',
-        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/7e1d52139832441.6236ffb0a0b0f.png'
-    },{
-        name: 'LogoFolio vol.1',
-        type: 'logos',
-        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e7ed19142445145.62672c99894b0.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3e87a7139345117.622e44eb4fc6a.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9fed28139345117.622e44eb50ba0.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/8f1ce4139345117.622e44eb4f0c8.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a2a511139345117.622e44eb4f6c1.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/37cd5b139345117.622e44eb51888.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/2fddcf139345117.622e44eb506b9.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3db08b139345117.622e44eb51334.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/97a698139345117.622e44eb51e08.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/42e827139345117.622e44eb50196.png'
+        ]
+    },
+    {
+        name: '36 Days Od Type',
+        type: 'lettering',
+        breif: "",
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/c661e9141106279.624da2c7bb96b.png',
         images: [
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
-            'https://mir-s3-cdn-cf.behance.net/project_modules/fs/37cd5b139345117.622e44eb51888.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b3da1d141106279.624da2c7bc2d8.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/45102d141106279.624da2c411fd5.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/60f178141106279.624da2c412a81.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0bae84141106279.624da979a8ff1.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/bdec13141106279.624da2c5312b0.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3e5945141106279.624da2c56554e.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/635139141106279.624da2c565d41.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/cbf3ed141106279.624da2c5afc63.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3089e4141106279.624da2c5af427.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/fb4688141106279.624da2c7b74bc.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/be3d88141106279.624da2c6076ae.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/01f578141106279.624da2c6080c1.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/6d91ef141106279.624da2c649094.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/805a5b141106279.624da2c649d71.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/41384a141106279.624da2c6b18bc.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f3afa6141106279.624da2c6b0eb1.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d04c68141106279.624da2c71c585.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/ef58dc141106279.624da2c71be13.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9243f5141106279.624da2c75d072.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/c661e9141106279.624da2c7bb96b.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/2ea596141106279.624da2c7b9b4d.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/c8e841141106279.624da2c7b7c1c.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/325933141106279.624da2c7ba9fc.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/bf96b0141106279.624da2c7bb259.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/73cf8e141106279.624da2c7b9052.png',
+            'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d8c6ac141106279.624da2c7b85ad.png'
         ]
     }
 ]
